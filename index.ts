36372304c51b8a981aa51f277703d038f29d01b8
Marius Kehl <80584154+MeroFuruya@users.noreply.github.com>
2023-04-19 17:04:05 +0200

    changed filename

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ async function main() {
 
     const res = folderStructure.findDoubleFilesByHash();
     
-    fs.writeFile("double_folders.json", JSON.stringify(res, null, 2), { encoding: "utf-8" }, (err) => { if (err) console.error(err) });
+    fs.writeFile("multiplied_files.json", JSON.stringify(res, null, 2), { encoding: "utf-8" }, (err) => { if (err) console.error(err) });
 }
 
 main().catch(console.error);
